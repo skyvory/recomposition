66e21f33749799ebbb32dd95a7798366f15d17cf
Skyvory <skyvory@hotmail.com>
2016-02-26 14:38:23 +0800

    Provide toastService for notification

--- app/services/vnService.js
+++ app/services/vnService.js
@@ -12,3 +12,49 @@ vnServices.factory('Vn', ['$resource', function($resource) {
 		}
 	});
 }]);
+
+vnServices.factory('toastService', ['$mdToast', function($mdToast) {
+	var toast_content = [];
+	function toast() {
+		$mdToast.show(
+			$mdToast.simple()
+				.content(toast_content[0])
+				.position('bottom left')
+				.hideDelay(0)
+				.theme("success-toast")
+		).then(function() {
+			toast_content.splice(0, 1);
+			if(toast_content.length) {
+				toast();
+			}
+		});
+	}
+	var toastMethod = {
+		show: function(text_content) {
+			toast_content = text_content;
+			$mdToast.show(
+				$mdToast.simple()
+					.content(text_content)
+					.position('bottom left')
+					.hideDelay(0)
+					.theme("success-toast")
+			);
+		},
+		hide: function() {
+			$mdToast.hide();
+		},
+		updateContent: function(text_content) {
+			$mdToast.updateTextContent(text_content);
+		},
+		pop: function(text_content) {
+			if(toast_content.length) {
+				toast_content.push(text_content);
+			}
+			else {
+				toast_content.push(text_content);
+				toast();
+			}
+		},
+	}
+	return toastMethod;
+}]);
