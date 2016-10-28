b902b7a66119126d4a765fd354d353e27aad9ebc
Skyvory <skyvory@hotmail.com>
2016-10-28 16:58:46 +0800

    Disable auth.guard in routing temporarily

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,8 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { AuthGuard } from './common/auth.guard';
 
-import { AuthGuard } from './guards/auth.guard';
+// import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login.component';
 import { HomeComponent } from './home.component';
 import { VnComponent } from './vn.component';
