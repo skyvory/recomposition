24b71280b7488201cccff3cdb5858e6081e3ee79
Skyvory <skyvory@hotmail.com>
2016-11-02 16:12:12 +0800

    Clean up vn.service garbage code

--- app/vn.service.ts
+++ app/vn.service.ts
@@ -6,7 +6,6 @@ import 'rxjs/add/operator/catch';
 // import 'rxjs/add/operator/throw';
 
 import { contentHeaders } from './common/headers';
-// import { AuthenticationService } from './authentication.service';
 import { AuthHttp } from 'angular2-jwt';
 
 
@@ -15,16 +14,12 @@ export class VnService {
 	constructor(
 		public authHttp: AuthHttp,
 		private http: Http,
-		// private authenticationService: AuthenticationService,
 	) {}
 
 	getVns(): Observable<any> {
 		return this.authHttp.get('http://localhost/record/public/api/vn', { headers: contentHeaders })
 			.map(
 				(response:Response) => {
-					// console.log("RAW", response);
-					// console.log("JSON", response.json());
-					// data => data.json()
 					return response.json();
 				},
 				err => console.warn(err)
@@ -33,25 +28,5 @@ export class VnService {
 				(error:any) => Observable.throw(error.json().error || 'Server error')
 			)
 		;
-
-		// return this.authHttp.get('http://localhost/record/public/api/vn', { headers: contentHeaders })
-		// 	.map((response:Response) => {
-		// 		console.log(response);
-		// 		return response;
-		// 	})
-
-		// return this.authHttp.get('http://localhost/record/public/api/vn', { headers: contentHeaders })
-		// 	.map(
-		// 	data => {
-		// 		console.log("VN", data);
-		// 	},
-		// 	err => console.log(err),
-		// 	() => console.log("Request complete")
-		// );
-		// let headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
-		// let options = new RequestOptions({ headers: headers });
-
-		// return this.http.get('http://localhost/record/public/api/vn', options)
-		// 	.map((response: Response) => response.json());
 	}
 }
