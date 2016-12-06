bb58b18c9de9ba9b7f59b08d312e0764c00bc488
Skyvory <skyvory@hotmail.com>
2016-12-06 15:31:52 +0800

    Separate app template to its own html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	template: `
-	<md-sidenav-layout>
-		<md-sidenav #start (open)="closeStartButton.focus()">
-			Navigation Menu
-			<br>
-			<a md-button routerLink="/home" routerLinkActive="active">Home</a>
-			<br>
-			<a md-button routerLink="/vn" routerLinkActive="active">VN</a>
-			<br>
-			<button md-button #closeStartButton (click)="start.close()">Close</button>
-
-		</md-sidenav>
-	   
-		<button md-button (click)="start.open()">Menu</button>
-
-		<router-outlet></router-outlet>
-	  </md-sidenav-layout>
-	`,
+	templateUrl: './app.component.html',
 	// directives: [ROUTER_DIRECTIVES]
 })
 
