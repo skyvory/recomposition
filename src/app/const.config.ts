0b26808d64b0a5faba72abda6895ddf05dd16481
Skyvory <skyvory@hotmail.com>
2016-12-14 15:27:31 +0800

    Provide constant for API path to backend service

--- src/app/const.config.ts
+++ src/app/const.config.ts
@@ -1,3 +1,12 @@
 export class Constant {
 	public static get USE_ANGULAR2JWT():boolean { return false; }
+	public static get API_PATH():string {
+		let env = 'localhost';
+		if(env === 'localhost') {
+			return 'http://localhost/record/public/api/';
+		}
+		else if(env === 'server') {
+			return 'https://skyvory.net/record/public/api/';
+		}
+	}
 }
