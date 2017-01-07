9d429e3ff5e883d5d010a210890a49c85984ba62
Skyvory <skyvory@hotmail.com>
2017-01-07 16:45:08 +0800

    Base draft of VN page

--- src/app/vn/vn.component.ts
+++ src/app/vn/vn.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { VnService } from '../vn.service';
 
 @Component({
-  selector: 'vn-selector',
-  templateUrl: './vn.component.html',
-  styleUrls: ['./vn.component.css']
+	selector: 'vn-selector',
+	templateUrl: './vn.component.html',
+	styleUrls: ['./vn.component.css']
 })
 export class VnComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private vnService: VnService
+	) { }
 
-  ngOnInit() {
-  }
+	vns: any = [];
+	ngOnInit() {
+		this.loadVns();
+	}
+
+	query:any = {
+		limit: 12,
+		page: 1,
+		filter: '',
+		total: 0
+	};
+
+	loadVns():void {
+		this.vnService.getVns(this.query.limit, this.query.page, this.query.filter).subscribe(response => {
+			this.vns = response.data;
+			this.query.total = response.total;
+		});
+	}
 
 }
