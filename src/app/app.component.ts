d7f33c4007fb8a05825f879fb3cc47b81283e65a
Rika Voice <skyvory@hotmail.com>
2017-02-16 19:23:41 +0800

    Display loading bar during wait in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 // import { ROUTER_DIRECTIVES } from '@angular/router';
 
 // import { LoginComponent } from './login.component';
+import { Router, Event as RouterEvent, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
@@ -9,6 +10,27 @@ import { Component } from '@angular/core';
 	// directives: [ROUTER_DIRECTIVES]
 })
 
-export class AppComponent{
+export class AppComponent {
 	title = "Recomposition";
+
+	loading: boolean = true;
+
+	constructor(
+		private router: Router
+	) {
+		router.events.subscribe((event: RouterEvent) => {
+			this.navigationInterceptor(event);
+		});
+	}
+
+	navigationInterceptor(event: RouterEvent): void {
+		if (event instanceof NavigationStart)
+			this.loading = true;
+		if (event instanceof NavigationEnd)
+			this.loading = false;
+		if (event instanceof NavigationCancel)
+			this.loading = false;
+		if (event instanceof NavigationError)
+			this.loading = false;
+	}
 }
