67bb086d96cff19c5039fd491958545d3e12ed33
Rika Voice <skyvory@hotmail.com>
2017-04-28 16:10:59 +0800

    GC: remove garbage comments and position reorder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
+import { AuthGuard } from './guards/auth.guard';
+import './rxjs-extensions';
+import { StaffOnlyGuard } from './guards/staff-only.guard';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { Ng2PaginationModule } from 'ng2-pagination';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ClipboardModule } from 'ngx-clipboard';
+
+import { LoginModule } from './login/login.module';
 import { HomeModule } from './+home/home.module';
 import { SettingModule } from './+setting/setting.module';
 import { AssessmentModule } from './+assessment/assessment.module';
 import { VnModule } from './vn/vn.module';
+import { SidebarsharedModule } from './sidebar/sidebar-shared.module';
 
 import { AuthenticationService } from './authentication.service';
 import { VnService } from './vn.service';
@@ -20,29 +31,10 @@ import { CharacterService } from './character.service';
 import { LineamentService } from './lineament.service';
 import { ActiveService } from './active.service';
 import { FileUploadService } from './file-upload.service';
-
-import { AuthGuard } from './guards/auth.guard';
-import { LoginModule } from './login/login.module';
-// import { VnComponent } from './vn.component';
-// import { VnFillComponent } from './vn-fill.component';
-// import { VnAssessmentComponent } from './vn-assessment.component';
-// import { VnCharacterComponent } from './+vn-character/vn-character.component';
-import { SearchComponent } from './+search/search.component';
-
-import './rxjs-extensions';
-
-// import { AUTH_PROVIDERS } from 'angular2-jwt';
-// for angular2-jwt configuration
-// import { provideAuth } from 'angular2-jwt';
-import { MaterialModule } from '@angular/material';
-import { Ng2PaginationModule } from 'ng2-pagination';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { SidebarsharedModule } from './sidebar/sidebar-shared.module';
 import { ToastService } from './toaster/toast.service';
 import { SettingService } from './setting.service';
-import { ClipboardModule } from 'ngx-clipboard';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StaffOnlyGuard } from './guards/staff-only.guard';
+
+import { SearchComponent } from './+search/search.component';
 
 @NgModule({
 	imports: [
@@ -50,28 +42,23 @@ import { StaffOnlyGuard } from './guards/staff-only.guard';
 		FormsModule,
 		HttpModule,
 		routing,
-		HomeModule,
-		MaterialModule,
-		Ng2PaginationModule,
-		SettingModule,
-		AssessmentModule,
-		VnModule,
-		FlexLayoutModule,
-		SidebarsharedModule,
-		ClipboardModule,
 		BrowserAnimationsModule,
-		LoginModule
+		MaterialModule,
+		FlexLayoutModule,
+		Ng2PaginationModule,
+		LoginModule,
+		SidebarsharedModule,
+		HomeModule,
+		VnModule,
+		AssessmentModule,
+		SettingModule,
+		ClipboardModule
 	],
 	declarations: [
 		AppComponent,
-		// VnComponent,
-		// VnFillComponent,
-		// VnAssessmentComponent,
-		// VnCharacterComponent,
 		SearchComponent
 	],
 	providers: [
-		// AUTH_PROVIDERS,
 		AuthGuard,
 		StaffOnlyGuard,
 		AuthenticationService,
@@ -79,16 +66,12 @@ import { StaffOnlyGuard } from './guards/staff-only.guard';
 		DeveloperService,
 		VndbService,
 		AssessmentService,
-		// provideAuth({
-		// 	tokenName: "recomposition_token",
-		// 	noJwtError: false,
-		// }),
 		CharacterService,
 		LineamentService,
 		ActiveService,
 		FileUploadService,
-		ToastService,
-		SettingService
+		SettingService,
+		ToastService
 	],
 	bootstrap: [ AppComponent ]
 })
