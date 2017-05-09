61da868db0b20bc5cc4a4b03632799ad40072e97
Rika Voice <skyvory@hotmail.com>
2017-05-09 17:09:19 +0800

    UI tweak and whitespace cleanup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -53,11 +53,14 @@ export class LoginComponent implements OnInit {
 		this.preAuth();
 		this.loopCast();
 		// this.shardSequenceActivation();
-		this.init();
-		this.animate();
+		setTimeout(() => {
+			this.init();
+			this.animate();
+		}, 180 * 1000);
 	}
 
 	loginAccessVisibility: string = "visible";
+	particlesVisibility: string = "visible";
 
 	stateOfShard1: string = "inactive";
 	stateOfShard2: string = "inactive";
@@ -90,6 +93,7 @@ export class LoginComponent implements OnInit {
 		this.stateOfForm = this.stateOfForm == 'enter' ? 'leave' : 'enter';
 		this.shardSequenceActivation();
 		this.loginAccessVisibility = 'hidden';
+		this.particlesVisibility = 'hidden';
 	}
 
 	bg1style: any = '';
@@ -158,34 +162,34 @@ export class LoginComponent implements OnInit {
 	particles: any;
 	geometry: any;
 	materials: any = [];
-	parameters:any;
-	i:any;
-	h:any;
-	color:any;
-	sprite:any;
-	size:any;
-	mouseX:any = 0;
-	mouseY:any = 0;
+	parameters: any;
+	i: any;
+	h: any;
+	color: any;
+	sprite: any;
+	size: any;
+	mouseX: any = 0;
+	mouseY: any = 0;
 
-	windowHalfX:any = window.innerWidth / 2;
-	windowHalfY:any = window.innerHeight / 2;
+	windowHalfX: any = window.innerWidth / 2;
+	windowHalfY: any = window.innerHeight / 2;
 
 
-	posix:any = 20;
-	posiy:any = -230;
-	poziz:any = 0;
-	rotax:any = 7.7;
-	rotay:any = 3;
-	rotaz:any = 0;
+	posix: any = 20;
+	posiy: any = -230;
+	poziz: any = 0;
+	rotax: any = 7.7;
+	rotay: any = 3;
+	rotaz: any = 0;
 
-	particleCount:any = 10000;
+	particleCount: any = 1000;
 
 	init(): void {
 		// let container = document.createElement('div');
 		let container = document.getElementById('eye-field');
 		document.body.appendChild(container);
 
-		this.camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 10000);
+		this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
 		// this.camera.position.z = 900;
 		this.scene = new THREE.Scene();
 		// this.scene.fog = new THREE.FogExp2( 0x000000, 0.0008 );
@@ -195,7 +199,7 @@ export class LoginComponent implements OnInit {
 		let sprite1 = textureLoader.load('assets/images/snowflake.png')
 		let sprite2 = textureLoader.load('assets/images/snowflakes_PNG7585.png')
 
-		for(let i = 0; i < this.particleCount; i++) {
+		for (let i = 0; i < this.particleCount; i++) {
 			var vertex = new THREE.Vector3();
 			vertex.x = Math.random() * 2000 - 1000;
 			vertex.y = Math.random() * 2000 - 1000;
@@ -205,16 +209,16 @@ export class LoginComponent implements OnInit {
 		}
 
 		this.parameters = [
-					[ [0.90, 0.05, 0.5], sprite1, 10 ],
-					[ [1.0, 0.2, 0.5], sprite2, 20 ]
+			[[0.90, 0.05, 0.5], sprite1, 10],
+			[[1.0, 0.2, 0.5], sprite2, 20]
 		];
 
-		for(let i = 0; i < this.parameters.length; i++) {
+		for (let i = 0; i < this.parameters.length; i++) {
 			let color = this.parameters[i][0];
 			let sprite = this.parameters[i][1];
 			let size = this.parameters[i][2];
 
-			this.materials[i] = new THREE.PointsMaterial( { size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest:false, transparent: true } );
+			this.materials[i] = new THREE.PointsMaterial({ size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true });
 			this.materials[i].color.setHSL(color[0], color[1], color[2]);
 
 			this.particles = new THREE.Points(this.geometry, this.materials[i]);
@@ -226,38 +230,38 @@ export class LoginComponent implements OnInit {
 		}
 
 		let canvas = document.getElementById('three');
-		this.renderer = new THREE.WebGLRenderer({canvas: canvas, alpha:true, antialias: true});
-	this.renderer.setPixelRatio(window.devicePixelRatio);
-	this.renderer.setSize(window.innerWidth, window.innerHeight);
-	this.renderer.setClearColor(0x000000, 0);
-	container.appendChild(this.renderer.domElement);
+		this.renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true, antialias: true });
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.setClearColor(0x000000, 0);
+		container.appendChild(this.renderer.domElement);
 
-	// document.addEventListener('mousemove', this.onDocumentMouseMove, false);
-	// document.addEventListener('resize', this.onWindowResize, false);
-}
+		// document.addEventListener('mousemove', this.onDocumentMouseMove, false);
+		// document.addEventListener('resize', this.onWindowResize, false);
+	}
 
-onDocumentMouseMove(event) {
-	this.mouseX = event.clientX - this.windowHalfX;
-	this.mouseY = event.clientY - this.windowHalfY;
-}
+	onDocumentMouseMove(event) {
+		this.mouseX = event.clientX - this.windowHalfX;
+		this.mouseY = event.clientY - this.windowHalfY;
+	}
 
-onWindowResize(event) {
-	console.log("OK");
-	this.renderer.setSize(window.innerWidth, window.innerHeight);
-	let canv = document.getElementById('three');
-	canv.style.width = window.innerWidth + 'px';
-}
+	onWindowResize(event) {
+		console.log("OK");
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		let canv = document.getElementById('three');
+		canv.style.width = window.innerWidth + 'px';
+	}
 
-animate = () => {
-	requestAnimationFrame(this.animate);
-	this.render();
-}
+	animate = () => {
+		requestAnimationFrame(this.animate);
+		this.render();
+	}
 
-render():void {
-	var time = Date.now() * 0.00005;
+	render(): void {
+		var time = Date.now() * 0.00005;
 
-	// this.camera.position.x += ( this.mouseX - this.camera.position.x ) * 0.05;
-	// this.camera.position.y +=  ( - this.mouseY - this.camera.position.y ) * 0.05;
+		// this.camera.position.x += ( this.mouseX - this.camera.position.x ) * 0.05;
+		// this.camera.position.y +=  ( - this.mouseY - this.camera.position.y ) * 0.05;
 		this.camera.position.x = 20;
 		this.camera.position.y = -230;
 		this.camera.position.z = 0;
@@ -265,23 +269,21 @@ render():void {
 		this.camera.rotation.y = 3;
 		this.camera.rotation.z = 0;
 
-	// this.camera.lookAt(this.scene.position);
-	for(let i = 0; i<this.scene.children.length;i++) {
-		var object = this.scene.children[i];
-		if(object instanceof THREE.Points) {
-			object.rotation.x = - time * (i<4 ? i+1 : -(i+1) );
+		// this.camera.lookAt(this.scene.position);
+		for (let i = 0; i < this.scene.children.length; i++) {
+			var object = this.scene.children[i];
+			if (object instanceof THREE.Points) {
+				object.rotation.x = - time * (i < 4 ? i + 1 : -(i + 1));
+			}
 		}
+
+		for (let i = 0; i < this.materials.length; i++) {
+			this.color = this.parameters[i][0];
+			this.h = (360 * (this.color[0] + time) % 360) / 360;
+			this.materials[i].color.setHSL(this.h, this.color[1], this.color[2]);
+		}
+
+		this.renderer.render(this.scene, this.camera);
 	}
-
-	for(let i=0;i<this.materials.length;i++) {
-		this.color = this.parameters[i][0];
-		this.h = (360*(this.color[0] + time) % 360 ) / 360;
-		this.materials[i].color.setHSL(this.h, this.color[1], this.color[2]);
-	}
-
-	this.renderer.render(this.scene, this.camera);
-}
-
-
 
 }
