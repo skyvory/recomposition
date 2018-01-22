d5b01fd2c394529ae4727687327271b16e003c55
Rika Voice <skyvory@hotmail.com>
2018-01-22 15:33:20 +0800

    GC: Remove redundant Portal Search functions

--- src/app/vn/vn-fill/vn-fill.component.ts
+++ src/app/vn/vn-fill/vn-fill.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 import { VnService } from '../../vn.service';
 import { DeveloperService } from '../../developer.service';
-import { VndbService } from '../../vndb.service';
 // import { ToastService } from '../../toaster/toast.service';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef } from '@angular/core';
@@ -16,7 +15,6 @@ import { PortalSearchDialog } from '../portal-search-dialog/portal-search-dialog
 	selector: 'vn-new-selector',
 	templateUrl: './vn-fill.component.html',
 	styleUrls: ['./vn-fill.component.css']
-
 })
 
 export class VnFillComponent implements OnInit {
@@ -30,7 +28,6 @@ export class VnFillComponent implements OnInit {
 		public router: Router,
 		private vnService: VnService,
 		private developerService: DeveloperService,
-		private vndbService: VndbService,
 		private route: ActivatedRoute,
 		// private toast: ToastService
 		public dialog: MatDialog
@@ -124,270 +121,6 @@ export class VnFillComponent implements OnInit {
 			});
 	}
 
-	applyDestination(): void {
-		let vndb_vn = this.searchDestination.vndb;
-		let egs_game = this.searchDestination.egs;
-		let vndb_release = this.searchDestination.vndbRelease;
-		console.log(vndb_vn, egs_game, vndb_release);
-
-		if (!vndb_vn && !egs_game && !vndb_release) {
-			// this.toast.pop("You need to select at least one search result to apply");
-			return;
-		}
-
-		this.toggle.portalDestinationApplyButtonDisable = true;
-
-		let vndb_user_hash = localStorage.getItem('vndb_user_hash');
-		let vndb_pass_hash = localStorage.getItem('vndb_pass_hash');
-
-		if (vndb_vn) {
-			this.vn.title_original = vndb_vn.original ? vndb_vn.original : vndb_vn.title;
-			this.vn.title_romaji = vndb_vn.title ? vndb_vn.title : vndb_vn.original;
-			this.vn.alias = vndb_vn.aliases;
-			this.vn.date_release = vndb_vn.released;
-			this.vn.image = vndb_vn.image;
-			this.vn.vndb_vn_id = vndb_vn.id;
-		}
-
-		if (vndb_release) {
-			this.vn.title_original = vndb_release.original ? vndb_release.original : vndb_release.title;
-			this.vn.title_romaji = vndb_release.title ? vndb_release.title : vndb_release.original;
-			this.vn.date_release = vndb_release.released;
-			this.vn.vndb_release_id = vndb_release.id;
-		}
-
-		if (egs_game) {
-			this.vn.title_original = egs_game.gamename;
-			this.vn.title_furigana = egs_game.gamefurigana;
-			this.vn.date_release = this.vn.date_release != egs_game.sellday ? egs_game.sellday : this.vn.date_release;
-			this.vn.erogamescape_game_id = egs_game.id;
-			this.vn.homepage = egs_game.shoukai ? egs_game.shoukai : null;
-			this.vn.twitter = egs_game.twitter ? egs_game.twitter : null;
-		}
-
-
-		let devOrig = null, devFuri = null, devRoman = null;
-
-
-		// Request for VNDB Release if one vndb search result is selected as autofill source
-		let developerHandleOnVndb = ():Promise<any> => {
-			return new Promise<any>((resolve, reject) => {
-				if(vndb_vn) {
-					this.vndbService.getVndbRelease(vndb_vn.id, vndb_user_hash, vndb_pass_hash).subscribe(response => {
-						// this.toast.pop("VNDB Release retrieved");
-						let vndb_release = response.data.items['0'];
-						if(response.data.items) {
-							let toBreak = false;
-							for(let i in response.data.items) {
-								if(response.data.items[i].producers) {
-									for(let j in response.data.items[i].producers) {
-										if(response.data.items[i].producers[j].developer == true) {
-											devOrig = response.data.items[i].producers[j].original ? response.data.items[i].producers[j].original : response.data.items[i].producers[j].name;
-											devRoman = response.data.items[i].producers[j].original != null ? response.data.items[i].producers[j].name : response.data.items[i].producers[j].name;
-
-											toBreak = true;
-										}
-
-										if(toBreak == true) {
-											break;
-										}
-									}
-								}
-								if(toBreak == true) {
-									break;
-								}
-							}
-						}
-
-						resolve(true);
-					}),
-					(err) => reject(false);
-				}
-				else {
-					resolve(false);
-				}
-			});
-		}
-
-		// Apply developer property from producers array in selected vndb release search result
-		let developerHandleOnVndbRelease = (): Promise<any> => {
-			return new Promise<any>((resolve, reject) => {
-				if (vndb_release) {
-					let toBreak = false;
-					if (vndb_release.producers) {
-						for (let i in vndb_release.producers) {
-							if (vndb_release.producers[i].developer == true) {
-								devOrig = vndb_release.producers[i].original ? vndb_release.producers[i].original : vndb_release.producers[i].name;
-								devRoman = vndb_release.producers[i].original != null ? vndb_release.producers[i].name : vndb_release.producers[i].original;
-
-								toBreak = true;
-							}
-
-							if (toBreak == true) {
-								break;
-							}
-						}
-					}
-					resolve(true);
-				}
-				else {
-					resolve(false);
-				}
-			});
-		}
-
-		// Overwrite and fill Developer property from EGS search result if one of the results is selected
-		let developerHandleOnEgs = (): Promise<any> => {
-			return new Promise<any>((resolve, reject) => {
-				if (egs_game) {
-					if (vndb_vn) {
-						if (egs_game.brandname != devOrig) {
-							// this.toast.pop("Different VNDB Developer vs EGS Brandname");
-							devOrig = egs_game.brandname;
-						}
-					}
-					else {
-						devOrig = egs_game.brandname;
-					}
-					devFuri = egs_game.brandfurigana;
-					resolve(true);
-				}
-				else {
-					resolve(false);
-				}
-			});
-		}
-
-		developerHandleOnVndb().then(res => {
-			console.log("is vndb source selected", res);
-			developerHandleOnVndbRelease().then(res => {
-				console.log("is vndb release source selected", res);
-				developerHandleOnEgs().then(res => {
-					console.log("is egs source selected", res);
-					// Developer properties has been filled. Commence checking to database.
-					// this.toast.pop("checking dev in db...");
-					let check = this.checkDeveloper(devOrig);
-					check.then(dev => {
-						this.vn.developer_id = dev.id;
-						this.toggle.portalDestinationApplyButtonDisable = false;
-
-					},
-					reason => {
-						console.log("Fail reason", reason);
-						// this.toast.pop("Developer not found. Automatically creating Developer...");
-						let reg = this.createDeveloper(devOrig, devFuri, devRoman);
-						reg.then(dev => {
-							// this.toast.pop("developer automatically created and applied to this VN");
-							this.vn.developer_id = dev.id;
-							this.toggle.portalDestinationApplyButtonDisable = false;
-						},
-						fail => {
-							console.log("Fail in promise", fail);
-							this.toggle.portalDestinationApplyButtonDisable = false;
-						});
-					});
-				});
-			});
-		});
-	}
-
-	searchDestination: any = {
-		vndb: null,
-		egs: null,
-		vndbRelease: null
-	};
-	selectVndbDestination(event: any, vndbNode: any) {
-		if (event == false) {
-			this.searchDestination.vndb = null;
-			return;
-		}
-		for (let i = 0; i < this.portalSearch.vndb.length; i++) {
-			if (this.portalSearch.vndb[i].id != vndbNode.id) {
-				this.portalSearch.vndb[i].selected = false;
-			}
-		}
-		this.searchDestination.vndb = vndbNode;
-	}
-	selectEgsDestination(event: any, egsNode: any) {
-		if (event == false) {
-			this.searchDestination.egs = null;
-			return;
-		}
-		for (let i = 0; i < this.portalSearch.egs.length; i++) {
-			if (this.portalSearch.egs[i].id != egsNode.id) {
-				this.portalSearch.egs[i].selected = false;
-			}
-		}
-		this.searchDestination.egs = egsNode;
-	}
-
-	retrieveVndbVn():void {
-		this.toggle.vndbIdLoadButtonDisable = true;
-
-		let vndb_user_hash = localStorage.getItem('vndb_user_hash');
-		let vndb_pass_hash = localStorage.getItem('vndb_pass_hash');
-
-		this.vndbService.getVndbVn(this.vn.vndb_vn_id, vndb_user_hash, vndb_pass_hash).subscribe(response => {
-			// this.toast.pop("VNDB VN retrieved");
-			let vndb_vn = response.data.items['0'];
-			this.vn.title_original = vndb_vn.original ? vndb_vn.original : vndb_vn.title;
-			this.vn.title_romaji = vndb_vn.title ? vndb_vn.title : vndb_vn.original;
-			this.vn.alias = vndb_vn.aliases;
-			this.vn.date_release = vndb_vn.released;
-			this.vn.image = vndb_vn.image;
-		});
-
-		this.vndbService.getVndbRelease(this.vn.vndb_vn_id, vndb_user_hash, vndb_pass_hash).subscribe(response => {
-			// this.toast.pop("VNDB Release retrieved");
-			let vndb_release = response.data.items['0'];
-			if(response.data.items) {
-				let toBreak = false;
-				for(let i in response.data.items) {
-					if(response.data.items[i].producers) {
-						for(let j in response.data.items[i].producers) {
-							if(response.data.items[i].producers[j].developer == true) {
-								let devOrig = response.data.items[i].producers[j].original ? response.data.items[i].producers[j].original : response.data.items[i].producers[j].name;
-								let devRoman = response.data.items[i].producers[j].original != null ? response.data.items[i].producers[j].name : response.data.items[i].producers[j].name;
-								// this.toast.pop("Checking if Developer exist in database...");
-								let check = this.checkDeveloper(devOrig);
-								check.then(dev => {
-									this.vn.developer_id = dev.id;
-									this.toggle.vndbIdLoadButtonDisable = false;
-								},
-								reason => {
-									console.log("Fail reason", reason);
-									// this.toast.pop("Developer not found. Automatically creating Developer...");
-									let reg = this.createDeveloper(devOrig, '', devRoman);
-									reg.then(dev => {
-										// this.toast.pop("Developer automatically created and applied to this VN");
-										this.vn.developer_id = dev.id;
-										this.toggle.vndbIdLoadButtonDisable = false;
-									},
-									fail => {
-										console.log("fail in promise", fail);
-										this.toggle.vndbIdLoadButtonDisable = false;
-									});
-								});
-
-								toBreak = true;
-							}
-
-							if(toBreak == true) {
-								break;
-							}
-						}
-					}
-					if(toBreak == true) {
-						break;
-					}
-				}
-			}
-		},
-		fail => {
-			// this.toast.pop("Failed to retrieve from VNDB for some reason");
-		});
-	}
-
 	checkDeveloper(original: string): Promise<any> {
 		return new Promise<string>((resolve, reject) => {
 			this.developerService.searchDeveloper(original).subscribe(response => {
@@ -436,73 +169,10 @@ export class VnFillComponent implements OnInit {
 		});
 	}
 
-	portalSearchVn(search_query: string): void {
-		if (!localStorage.getItem('vndb_user_hash') || !localStorage.getItem('vndb_pass_hash')) {
-			// this.toast.pop("VNDB creential hasn't set yet");
-			return;
-		}
-		this.toggle.portalSearchButtonDisable = true;
-
-		this.vnService.portalSearchVn(search_query).subscribe(response => {
-			console.log(response);
-			this.portalSearch.vndb = response.data.vndb.items;
-			this.portalSearch.egs = response.data.egs;
-			// this.toast.pop("Portal search completed");
-			this.toggle.portalSearchButtonDisable = false;
-		});
-	}
-
-	portalSearch: any = {
-		vndb: [],
-		egs: [],
-		vndbRelease: []
-	};
-
-	retrieveVndbReleases(vndb_id: number, event: any): void {
-		// Disable the pressed button
-		event.target.disabled = true;
-
-		let vndb_user_hash = localStorage.getItem('vndb_user_hash');
-		let vndb_pass_hash = localStorage.getItem('vndb_pass_hash');
-
-		this.vndbService.getVndbRelease(vndb_id, vndb_user_hash, vndb_pass_hash).subscribe(response => {
-			// this.toast.pop("VNDB Release retrieved");
-			this.portalSearch.vndbRelease = response.data.items;
-			event.target.disabled = false;
-		});
-	}
-
-	selectVndbReleaseDestination(event: any, vndbReleaseNode: any) {
-		if (event == false) {
-			this.searchDestination.vndbRelease = null;
-			return;
-		}
-		for (let i = 0; i < this.portalSearch.vndbRelease.length; i++) {
-			if (this.portalSearch.vndbRelease[i].id != vndbReleaseNode.id) {
-				this.portalSearch.vndbRelease[i].selected = false;
-			}
-		}
-		this.searchDestination.vndbRelease = vndbReleaseNode;
-	}
-
-	mockindividual(event): void {
-		console.log(event);
-		event.target.disabled = true;
-		setTimeout(function () {
-			event.target.disabled = false;
-		}, 3000);
-	}
-
 	dialogConfig:MatDialogConfig = {
 		width: '90vw',
 		height: '90vh',
 		maxWidth: 'none',
-		// position: {
-		// 	top: '0',
-		// 	right: '0',
-		// 	bottom: '0',
-		// 	left: '0'
-		// }
 	}
 	
 	popPortalSearchDialog(): void {
